fdf8907faa6dcd4bfe740ae69ac708fcf2d7bb6a
IlGuru <s.busnelli@tin.it>
2015-09-18 13:56:00 +0200

    0002

--- main.c
+++ main.c
@@ -1,26 +1,38 @@
 #include <stdio.h>
 
-#define UINT_SIZE sizeof(unsigned int)
-
 unsigned char memory[4096];
 
 //----------------------------------------------------
 //	Screen
 
-#define scr_xmax 20
-#define scr_ymax 10
+#define scr_xmax 15
+#define scr_ymax 2
 
-unsigned char *screenMem;
-unsigned char *screenMem_Buf;
-unsigned char *screenMem_Reg;
-unsigned int  *screenMem_Reg_scr_x;
-unsigned int  *screenMem_Reg_scr_y;
+typedef struct {
+	//	Buffer
+	unsigned char 	Buffer[scr_xmax*scr_ymax];
+	//	Registri
+	short int	x_pos;
+	short int	y_pos;
+	short int	x_max;
+	short int	y_max;
+	short int	BufSize;
+} t_scrMem;
 
-void screenBuf_clear() {
+t_scrMem *scrMem;
+
+void scrMem_ClearBuf() {
 	int xg = 0;
 	for (xg = 0 ; xg < scr_xmax*scr_ymax ; xg++) {
-		screenMem_Buf[xg] = '\0';
+		scrMem->Buffer[xg] = '\0';
 	}
+}
+
+void scrMem_Init() {
+	scrMem->x_max		= scr_xmax;
+	scrMem->y_max		= scr_ymax;
+	scrMem->BufSize		= scr_xmax*scr_ymax;
+	scrMem_ClearBuf();
 }
 
 //----------------------------------------------------
@@ -32,64 +44,85 @@ void clrscr() {
 
 //----------------------------------------------------
 void scr_clear() {
-	screenMem_Reg_scr_x = 0;
-	screenMem_Reg_scr_y = 0;
+	scrMem->x_pos = 0;
+	scrMem->y_pos = 0;
 	clrscr();
 }
 
 void scr_write( char c ) {
 
-	if ( *screenMem_Reg_scr_x >= scr_xmax ) {
-		screenMem_Reg_scr_x = 0;
-		screenMem_Reg_scr_y++;
+	if ( scrMem->x_pos >= scr_xmax ) {
+		scrMem->x_pos = 0;
+		scrMem->y_pos++;
 		putchar('\n');
 		putchar('\r');
 	}		
-	if ( *screenMem_Reg_scr_y >= scr_ymax ) {
-		return;
+	if ( scrMem->y_pos < scr_ymax ) {
+		if ( c == '\0' ) c = ' ';
+		if ( c == '\n' ) scrMem->y_pos++;
+		if ( c == '\r' ) scrMem->x_pos = 0;
+
+		scrMem->x_pos++;
+		putchar( c );		
+	} else {
+		printf( "%02X ", ((int) c) );
 	}		
 
-	if ( c == '\0' ) c = ' ';
-	if ( c == '\n' ) screenMem_Reg_scr_y++;
-	if ( c == '\r' ) screenMem_Reg_scr_x = 0;
-
-	screenMem_Reg_scr_x++;
-	putchar( c );
 }
 
 //----------------------------------------------------
-static void doFrame(int amount) {
+static void scr_Repaint(int amount) {
 	int xg = 0;
 	
 	scr_clear();
 		
 	for (xg = 0 ; xg < amount ; xg++) {
-		scr_write(screenMem_Buf[xg]);
+		scr_write(scrMem->Buffer[xg]);		
 	}
 
 }
 
+void DumpMem( int x_from, int x_to ) {
+	int x,i;
+	printf("\nDump:\n");
+	printf( "BaseMem   %08X\n", memory );
+	printf( "ScreenMem %08X\n", scrMem );
+	
+	i=0;
+	for ( x=x_from; x<=x_to; x++ ) {
+		if ( i>15 ) {
+			printf("\n");
+			i=0;
+		}
+		if ( i==0 ) {
+			printf( "%04X: ", x );
+		}
+		printf( "%02X ", memory[x] );
+		i++;
+	}
+	printf("\n");
+}
+
 int main() {
 
-
-	screenMem 			= memory;
-	screenMem_Buf		= screenMem;
-	screenMem_Reg		= screenMem+(scr_xmax*scr_ymax);
-	screenMem_Reg_scr_x = (int*) screenMem_Reg;
-	screenMem_Reg_scr_x = (int*) screenMem_Reg + UINT_SIZE;
+	scrMem              = (t_scrMem*) memory;
+	scrMem_Init();
 	
-	screenBuf_clear();
+	scrMem->Buffer[0] = 'C';
+	scrMem->Buffer[1] = 'i';
+	scrMem->Buffer[2] = 'a';
+	scrMem->Buffer[3] = 'o';
+	scrMem->Buffer[4] = '.';
 	
-	screenMem_Buf[0] = 'C';
-	screenMem_Buf[1] = 'i';
-	screenMem_Buf[2] = 'a';
-	screenMem_Buf[3] = 'o';
-	screenMem_Buf[4] = '.';
+	scr_Repaint(scr_xmax*scr_ymax);
+	DumpMem( 0, sizeof(*scrMem) );
 	
-	scr_clear();
-//	doFrame(scr_xmax*(scr_ymax+1));
-	printf("\n%d",screenMem_Reg_scr_x);
-	printf("\n%d",screenMem_Reg_scr_y);
+	printf("\nRegistri:\n");
+	printf("\n%d",scrMem->x_pos);
+	printf("\n%d",scrMem->y_pos);
+	printf("\n%d",scrMem->x_max);
+	printf("\n%d",scrMem->y_max);
+	printf("\n%d",scrMem->BufSize);
 	
 	return 0;
 
